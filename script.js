5671464bc29e6d54e9944130b6b82d2d3d5f2891
Alexis-website <rudriyansh@gmail.com>
2025-03-11 14:30:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function navigate(page) {
+    if (page === 'chat.html') {
+        window.location.href = 'https://alexis-website.github.io/chat';
+    } else {
+        window.location.href = page;
+    }
+}
